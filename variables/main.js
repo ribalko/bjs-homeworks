2bfeca7d1f15fde6d5130ee20168bfd749d44929
Dmitriy Ribalko <ribalko@yandex.ru>
2019-03-01 23:58:25 +0300

    1,2 tasks

---
+++ variables/main.js
@@ -0,0 +1,24 @@
+// Task 1
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+
+// Task 2
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let disciplineCount = 3;
+
+average = (algebra + geography + physics)/disciplineCount;
+
+console.log(disciplineCount);
+
+
+// Task 3
+
