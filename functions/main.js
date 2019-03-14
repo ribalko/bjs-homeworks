d8d980055ee145eb0f62fe31fd4af664a47f16fd
Dmitriy Ribalko <ribalko@yandex.ru>
2019-03-15 01:50:59 +0300

    task 2 and task 3

--- functions/main.js
+++ functions/main.js
@@ -51,3 +51,93 @@ function showSolutionsMessage(a, b, c) {
     }
 
 }
+
+
+
+// Задача 2
+
+function getName(secretCode) {
+    if (secretCode == 0)
+        return 'Родриго';
+    else if (secretCode == 1)
+        return 'Эмильо';
+    else
+        return undefined;
+    
+}
+
+function getPersonData(secretData) {
+    firstName = getName(secretData.aaa);
+    lastName = getName(secretData.bbb);
+    let bandit = { firstName : firstName, lastName : lastName};
+    return bandit;
+}
+
+let secretData = { aaa: 0 , bbb: 1 };
+let bandit = getPersonData(secretData);
+for (let name in bandit) {
+    console.log(name + ': ' + bandit[name]);
+}
+
+
+
+// Задача 3
+
+function getAverageMark(marks){
+    
+    if (marks.length === 0) {
+        averageMark = 0;
+        return averageMark;
+    }
+       
+        
+    let targetCount = 5;
+    let sum = 0;
+
+    if (marks.length > targetCount) {
+        marks.splice(targetCount);    
+    }
+
+    for(let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    
+    averageMark = sum/marks.length; 
+
+    return averageMark;
+}
+
+function getAverageScoreData(data) {
+
+    let averageScores = {};
+    let disciplineCount = 0;
+    let disciplineSum = 0;
+    
+    for (let discipline in data) {
+        let marks = data[discipline];
+        let averageMark = getAverageMark(marks);
+        averageScores[discipline] = averageMark;
+        disciplineCount += 1;
+        disciplineSum += averageMark;
+    }  
+
+    let average = (disciplineCount === 0) ? 0 : disciplineSum/disciplineCount;
+    averageScores[discipline] = average;
+
+    return averageScores;
+}
+
+let data = {
+    algebra: [5,3,4,2,5],
+    geometry: [3,3,4,4,5],
+    russian: [5,5,4,5,5],
+    physics: [5,3,4,5],
+    music: [3,4,5],
+    english: [5,5,4,5,5,1]    
+};
+
+let averageData = getAverageScoreData(data);
+
+for (let discipline in averageData) {
+    console.log(discipline + ': ' + averageData[discipline]);
+}
